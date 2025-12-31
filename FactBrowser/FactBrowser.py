c0867a56663c794cedb57fa690f95ce0bb706ef9
ivanliu1003 <liuzhaoheng079@gmail.com>
2026-01-01 03:33:00 +0800

    add local save feature

--- FactBrowser/FactBrowser.py
+++ FactBrowser/FactBrowser.py
@@ -1,5 +1,26 @@
 import requests
-data=requests.get("https://uselessfacts.jsph.pl/api/v2/facts/random")
-data=data.json()
+def openfile():
+    with open("TextFile1.txt","r")as file:
+        print(file.read())
+with open("TextFile1.txt","r")as f:
+    seen=set()
+    for line in f:
+        seen.add(line.strip()+'\n')
 
-print(data["text"])
+
+
+    
+with open("TextFile1.txt","r+")as file:
+    for i in range(5):
+        data=requests.get("https://uselessfacts.jsph.pl/api/v2/facts/random")
+        data=data.json()
+        
+        if(data["text"] not in seen):
+            seen.add(data["text"].strip()+'\n')
+            print(data["text"])
+        else:
+            print("ivan is super smart")
+            break
+    for line in seen:
+        file.write(line.strip()+'\n')
+openfile()
